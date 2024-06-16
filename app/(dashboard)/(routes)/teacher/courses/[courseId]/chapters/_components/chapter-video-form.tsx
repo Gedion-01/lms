822a844fd421782a359fd58c4fb94f41340b49d9
gedion <117908662+Gedion-01@users.noreply.github.com>
2024-06-16 15:29:11 +0300

    update

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/_components/chapter-video-form.tsx
@@ -2,6 +2,7 @@
 
 import * as z from "zod";
 import axios from "axios";
+import MuxPlayer from "@mux/mux-player-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -11,7 +12,7 @@ import { Pencil, PlusCircle, Video } from "lucide-react";
 import { Chapter, MuxData } from "@prisma/client";
 import { FileUpload } from "@/components/file-upload";
 
-interface ChapterVideoProps {
+interface ChapterVideoFormProps {
   initialData: Chapter & { muxData?: MuxData | null };
   courseId: string;
   chapterId: string;
@@ -21,11 +22,11 @@ const formSchema = z.object({
   videoUrl: z.string().min(1),
 });
 
-export default function ChapterVideo({
+export default function ChapterVideoForm({
   initialData,
   courseId,
   chapterId,
-}: ChapterVideoProps) {
+}: ChapterVideoFormProps) {
   const [isEditing, setIsEditing] = useState(false);
 
   const toggleEdit = () => setIsEditing((current) => !current);
@@ -48,7 +49,7 @@ export default function ChapterVideo({
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Course image
+        Chapter video
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing && <>Cancel</>}
           {!isEditing && !initialData.videoUrl && (
@@ -71,12 +72,14 @@ export default function ChapterVideo({
             <Video className="h-10 w-10 text-slate-500" />
           </div>
         ) : (
-          <div className="relative aspect-video mt-2">Video uploaded</div>
+          <div className="relative aspect-video mt-2">
+            <MuxPlayer playbackId={initialData?.muxData?.playbackId || ""} />
+          </div>
         ))}
       {isEditing && (
         <div>
           <FileUpload
-            endpoint="courseImage"
+            endpoint="chapterVideo"
             onChange={(url) => {
               if (url) {
                 onSubmit({ videoUrl: url });
---
+++ components/banner.tsx
@@ -0,0 +1,10 @@
+import { AlertTriangle, CheckCircleIcon } from "lucide-react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const bannerVariants = cva()
+
+export default function Banner() {
+  return <div>Banner</div>;
+}
