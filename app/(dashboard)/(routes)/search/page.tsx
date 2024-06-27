3946745fdc59b7bda0d444bedbc24fa3fb234ef6
gedion <117908662+Gedion-01@users.noreply.github.com>
2024-06-27 19:10:58 +0300

    update

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,3 +1,14 @@
-export default function SearchPage() {
-  return <p>this is a search page</p>;
+import { db } from "@/lib/db";
+
+export default async function SearchPage() {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  return (
+    <div className="p-6">
+      <Categories />
+    </div>
+  )
 }
