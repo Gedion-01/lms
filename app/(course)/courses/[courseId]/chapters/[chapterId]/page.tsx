07a5d647837068cc85a70e09a270fde5c2004100
gedion <117908662+Gedion-01@users.noreply.github.com>
2024-07-06 12:44:54 +0300

    added getchapter action

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,5 +1,35 @@
-export default function ChapterIdPage() {
-    return (
-        <div>Chapter id</div>
-    )
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { getChapter } from "@/actions/get-chapter";
+
+export default async function ChapterIdPage({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+  return <div>Chapter id</div>;
 }
