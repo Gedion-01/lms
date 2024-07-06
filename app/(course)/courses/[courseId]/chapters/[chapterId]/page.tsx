5390f394aead655ea044bca82798d5832021fc32
gedion <117908662+Gedion-01@users.noreply.github.com>
2024-07-06 14:33:48 +0300

    video player creation started in the chapter page

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -2,6 +2,9 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 import { getChapter } from "@/actions/get-chapter";
+import Banner from "@/components/banner";
+
+import { VideoPlayer } from "./_components/video-player";
 
 export default async function ChapterIdPage({
   params,
@@ -31,5 +34,34 @@ export default async function ChapterIdPage({
   if (!chapter || !course) {
     return redirect("/");
   }
-  return <div>Chapter id</div>;
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner variant="success" label="You already completed this chapter." />
+      )}
+      {isLocked && (
+        <Banner
+          variant="warning"
+          label="You need to purchase this course to watch this chapter."
+        />
+      )}
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4">
+          <VideoPlayer
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
