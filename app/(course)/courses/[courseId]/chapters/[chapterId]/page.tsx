87eef314e995e89076fad2121b164853e3ab968e
gedion <117908662+Gedion-01@users.noreply.github.com>
2024-07-07 11:30:01 +0300

    ui updated & stripe integration

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -3,9 +3,12 @@ import { redirect } from "next/navigation";
 
 import { getChapter } from "@/actions/get-chapter";
 import Banner from "@/components/banner";
+import { Separator } from "@/components/ui/separator";
+import { File } from "lucide-react";
 
 import { VideoPlayer } from "./_components/video-player";
 import { CourseEnrollButton } from "./_components/course-enroll-button";
+import { Preview } from "@/components/preview";
 
 export default async function ChapterIdPage({
   params,
@@ -73,6 +76,28 @@ export default async function ChapterIdPage({
             />
           )}
         </div>
+        <Separator />
+        <div>
+          <Preview value={chapter.description!} />
+        </div>
+        {!!attachments.length && (
+          <>
+            <Separator />
+            <div className="p-4">
+              {attachments.map((attachment) => (
+                <a
+                  href={attachment.url}
+                  target="_blank"
+                  key={attachment.id}
+                  className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+                >
+                  <File />
+                  <p className="line-clamp-1">{attachment.name}</p>
+                </a>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
